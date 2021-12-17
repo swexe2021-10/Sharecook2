bfed91038627e2b76183de79ca9045d3ed09a9b5
Hidaka Yuuta <1911140073f@ed.fuk.kindai.ac.jp>
2021-12-17 15:18:48 +0000

    showの編集

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_17_081722) do
+ActiveRecord::Schema.define(version: 2021_12_17_092207) do
 
   create_table "comments", force: :cascade do |t|
     t.string "comment_content"
